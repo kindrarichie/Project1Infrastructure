bb201cfbdc28e3385f70c9ddb60b2b9e0900733d
kindrarichie <kindra.richie.714@my.csun.edu>
2019-10-23 11:46:19 -0700

    main.tf

---
+++ Terraform/SetupTerraformBackend/main.tf
@@ -0,0 +1,43 @@
+# Establish and configure Project 1 Terraform Locking State in S3
+
+
+# configure provider with proper credentials 
+provider "aws" {
+  version = "~> 2.33"
+  region = "us-west-2"
+  shared_credentials_file = "/Users/kindra/.aws/credentials"
+  profile = "default"
+} # end provider
+
+
+# create an s3 bucket
+resource "aws_s3_bucket" "tf-remote-state" {
+  bucket = "project1-rstate"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# create the dynamoDB table
+
+resource "aws_dynamodb_table" "dynamodb-tf-state-lock" {
+  name            = "project1-tfslock"
+  hash_key        = "LockID"
+  read_capacity   = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "DynamoDB tf state locking"
+  }
+} 
+
