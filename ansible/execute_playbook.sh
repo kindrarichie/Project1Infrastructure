2bba4b1411c6ce716033a1ccd146bbaad1157669
alexcoward <alexander.coward.17@my.csun.edu>
2019-11-06 10:03:31 -0800

    test new ansible file

--- ansible/execute_playbook.sh
+++ ansible/execute_playbook.sh
@@ -5,15 +5,15 @@
 # mssh ubuntu@[i-XXXX] "bash -s" < ~/Desktop/execute_playbook.sh
 #
 #Enter The AWS instance id of Server1 [i-XXXX]
-ID1="i-04fa8c376fb0e16e7"
+ID1="i-0354c27a504a93f5b"
 #Enter The AWS instance DNS of Server1 [EC2.PUBLIC.DNS]
-DNS1="ip-172-31-160-190.us-west-2.compute.internal"
+DNS1="ip-172-31-173-142.us-west-2.compute.internal"
 #Enter The AWS availability zone of Server1 [us-west-2c]
 AZ1="us-west-2b"
 #Enter The AWS instance id of Server2 [i-XXXX]
-ID2="i-0ba949b26887296bf"
+ID2="i-0e30d812b31669aae"
 #Enter The AWS instance DNS of Server2 [EC2.PUBLIC.DNS]
-DNS2="ip-172-31-208-120.us-west-2.compute.internal"
+DNS2="ip-172-31-201-121.us-west-2.compute.internal"
 #Enter The AWS availability zone of Server2 [us-west-2c]
 AZ2="us-west-2c"
 #AWS CLI variables
@@ -32,7 +32,7 @@ yes y |ssh-keygen -q -t rsa -f my_rsa_key -N '' >/dev/null
 aws ec2-instance-connect send-ssh-public-key --region us-west-2 --instance-id "$ID1" --instance-os-user ubuntu \
 --availability-zone "$AZ1" --ssh-public-key file://my_rsa_key.pub && aws ec2-instance-connect send-ssh-public-key \
 --region us-west-2 --instance-id "$ID2" --instance-os-user ubuntu --availability-zone "$AZ2" --ssh-public-key file://my_rsa_key.pub \
-&& ansible-playbook -i /home/ubuntu/Project1Infrastructure/ansible/hosts /home/ubuntu/Project1Infrastructure/ansible/project1.yml
+&& ansible-playbook /home/ubuntu/Project1Infrastructure/ansible/setup_all.yml
 #cleanup
 rm -rf Project1Infrastructure/
 rm -f my_rsa_key
